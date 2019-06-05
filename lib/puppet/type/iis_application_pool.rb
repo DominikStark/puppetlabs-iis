d4bf5b6a8e6c9cba62d9ca1a97266ad29d5029a0
Dominik Stark <dominik.stark@ert.com>
2019-06-05 14:02:32 +0200

    changed property to param

--- lib/puppet/type/iis_application_pool.rb
+++ lib/puppet/type/iis_application_pool.rb
@@ -126,7 +126,7 @@ Puppet::Type.newtype(:iis_application_pool) do
     desc "Configure environment variables for the application pool."
   end
 
-  newproperty(:test) do
+  newparam(:test) do
     desc "Configure environment variables for the application pool."
   end
 
